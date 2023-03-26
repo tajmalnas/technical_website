184265d878bca43a69b5a4deb40b40d775275143
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-03-26 16:02:37 +0530

    made some changes in cards

---
+++ src/components/Basic.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Basic.css'
+
+const Basic = () => {
+  return (
+    <div>
+        <div class="wrapper four">
+            <div class="type">
+                <h3 class="typing">To become a great programmer it is not neccessary that how many languages you know.
+                    <br/>Actually in how much depth that language you know.
+                </h3>
+            </div>
+        </div>
+        <div className='glow'>
+            Start Your Coding Journey By Choosing Any One Of This
+        </div>
+    </div>
+  )
+}
+
+export default Basic
---
+++ src/components/Cards.jsx
@@ -0,0 +1,233 @@
+import React from 'react'
+import './Cards.css'
+const Cards = () => {
+  return (
+    <div className='ancestor'>
+        <div className='rows' style={{paddingLeft:'26px'}}>
+            <div className='card'>
+                <div class="container noselect">
+                    <div class="canvas">
+                        <div class="tracker tr-2"></div>
+                        <div class="tracker tr-1"></div>
+                        <div class="tracker tr-3"></div>
+                        <div class="tracker tr-4"></div>
+                        <div class="tracker tr-5"></div>
+                        <div class="tracker tr-6"></div>
+                        <div class="tracker tr-7"></div>
+                        <div class="tracker tr-8"></div>
+                        <div class="tracker tr-9"></div>
+                        <div class="tracker tr-10"></div>
+                        <div class="tracker tr-11"></div>
+                        <div class="tracker tr-12"></div>
+                        <div class="tracker tr-13"></div>
+                        <div class="tracker tr-14"></div>
+                        <div class="tracker tr-15"></div>
+                        <div class="tracker tr-16"></div>
+                        <div class="tracker tr-17"></div>
+                        <div class="tracker tr-18"></div>
+                        <div class="tracker tr-19"></div>
+                        <div class="tracker tr-20"></div>
+                        <div class="tracker tr-21"></div>
+                        <div class="tracker tr-22"></div>
+                        <div class="tracker tr-23"></div>
+                        <div class="tracker tr-24"></div>
+                        <div class="tracker tr-25"></div>
+                    <div id="card">
+                    <p id="prompt">C++</p>
+                    <div class="title"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png" height={'65px'}/></div>
+                </div>
+             </div>
+            </div>
+            </div>
+            <div className='card'>
+                <div class="container noselect">
+                    <div class="canvas">
+                        <div class="tracker tr-2"></div>
+                        <div class="tracker tr-1"></div>
+                        <div class="tracker tr-3"></div>
+                        <div class="tracker tr-4"></div>
+                        <div class="tracker tr-5"></div>
+                        <div class="tracker tr-6"></div>
+                        <div class="tracker tr-7"></div>
+                        <div class="tracker tr-8"></div>
+                        <div class="tracker tr-9"></div>
+                        <div class="tracker tr-10"></div>
+                        <div class="tracker tr-11"></div>
+                        <div class="tracker tr-12"></div>
+                        <div class="tracker tr-13"></div>
+                        <div class="tracker tr-14"></div>
+                        <div class="tracker tr-15"></div>
+                        <div class="tracker tr-16"></div>
+                        <div class="tracker tr-17"></div>
+                        <div class="tracker tr-18"></div>
+                        <div class="tracker tr-19"></div>
+                        <div class="tracker tr-20"></div>
+                        <div class="tracker tr-21"></div>
+                        <div class="tracker tr-22"></div>
+                        <div class="tracker tr-23"></div>
+                        <div class="tracker tr-24"></div>
+                        <div class="tracker tr-25"></div>
+                    <div id="card">
+                    <p id="prompt">Java</p>
+                    <div class="title"><img src="https://www.svgrepo.com/download/184143/java.svg" height={'75px'}/></div>
+                    
+                </div>
+             </div>
+            </div>
+            </div>
+            <div className='card'>
+                <div class="container noselect">
+                    <div class="canvas">
+                        <div class="tracker tr-2"></div>
+                        <div class="tracker tr-1"></div>
+                        <div class="tracker tr-3"></div>
+                        <div class="tracker tr-4"></div>
+                        <div class="tracker tr-5"></div>
+                        <div class="tracker tr-6"></div>
+                        <div class="tracker tr-7"></div>
+                        <div class="tracker tr-8"></div>
+                        <div class="tracker tr-9"></div>
+                        <div class="tracker tr-10"></div>
+                        <div class="tracker tr-11"></div>
+                        <div class="tracker tr-12"></div>
+                        <div class="tracker tr-13"></div>
+                        <div class="tracker tr-14"></div>
+                        <div class="tracker tr-15"></div>
+                        <div class="tracker tr-16"></div>
+                        <div class="tracker tr-17"></div>
+                        <div class="tracker tr-18"></div>
+                        <div class="tracker tr-19"></div>
+                        <div class="tracker tr-20"></div>
+                        <div class="tracker tr-21"></div>
+                        <div class="tracker tr-22"></div>
+                        <div class="tracker tr-23"></div>
+                        <div class="tracker tr-24"></div>
+                        <div class="tracker tr-25"></div>
+                    <div id="card">
+                    <p id="prompt">Python</p>
+                    <div class="title"><img src="https://cdn3d.iconscout.com/3d/premium/thumb/python-6815592-5602757.png?f=webp" height={'75px'} /></div>
+
+                </div>
+             </div>
+            </div>
+            </div>
+            <div className='card'>
+                <div class="container noselect">
+                    <div class="canvas">
+                        <div class="tracker tr-2"></div>
+                        <div class="tracker tr-1"></div>
+                        <div class="tracker tr-3"></div>
+                        <div class="tracker tr-4"></div>
+                        <div class="tracker tr-5"></div>
+                        <div class="tracker tr-6"></div>
+                        <div class="tracker tr-7"></div>
+                        <div class="tracker tr-8"></div>
+                        <div class="tracker tr-9"></div>
+                        <div class="tracker tr-10"></div>
+                        <div class="tracker tr-11"></div>
+                        <div class="tracker tr-12"></div>
+                        <div class="tracker tr-13"></div>
+                        <div class="tracker tr-14"></div>
+                        <div class="tracker tr-15"></div>
+                        <div class="tracker tr-16"></div>
+                        <div class="tracker tr-17"></div>
+                        <div class="tracker tr-18"></div>
+                        <div class="tracker tr-19"></div>
+                        <div class="tracker tr-20"></div>
+                        <div class="tracker tr-21"></div>
+                        <div class="tracker tr-22"></div>
+                        <div class="tracker tr-23"></div>
+                        <div class="tracker tr-24"></div>
+                        <div class="tracker tr-25"></div>
+                    <div id="card">
+                    <p id="prompt">HOVER OVER :D</p>
+                    <div class="title">look mom,<br/>no JS</div>
+                    <div class="subtitle">
+                        mouse hover tracker
+                    </div>
+                </div>
+             </div>
+            </div>
+            </div>
+            <div className='card'>
+                <div class="container noselect">
+                    <div class="canvas">
+                        <div class="tracker tr-2"></div>
+                        <div class="tracker tr-1"></div>
+                        <div class="tracker tr-3"></div>
+                        <div class="tracker tr-4"></div>
+                        <div class="tracker tr-5"></div>
+                        <div class="tracker tr-6"></div>
+                        <div class="tracker tr-7"></div>
+                        <div class="tracker tr-8"></div>
+                        <div class="tracker tr-9"></div>
+                        <div class="tracker tr-10"></div>
+                        <div class="tracker tr-11"></div>
+                        <div class="tracker tr-12"></div>
+                        <div class="tracker tr-13"></div>
+                        <div class="tracker tr-14"></div>
+                        <div class="tracker tr-15"></div>
+                        <div class="tracker tr-16"></div>
+                        <div class="tracker tr-17"></div>
+                        <div class="tracker tr-18"></div>
+                        <div class="tracker tr-19"></div>
+                        <div class="tracker tr-20"></div>
+                        <div class="tracker tr-21"></div>
+                        <div class="tracker tr-22"></div>
+                        <div class="tracker tr-23"></div>
+                        <div class="tracker tr-24"></div>
+                        <div class="tracker tr-25"></div>
+                    <div id="card">
+                    <p id="prompt">HOVER OVER :D</p>
+                    <div class="title">look mom,<br/>no JS</div>
+                    <div class="subtitle">
+                        mouse hover tracker
+                    </div>
+                </div>
+             </div>
+            </div>
+            </div>
+            <div className='card'>
+                <div class="container noselect">
+                    <div class="canvas">
+                        <div class="tracker tr-2"></div>
+                        <div class="tracker tr-1"></div>
+                        <div class="tracker tr-3"></div>
+                        <div class="tracker tr-4"></div>
+                        <div class="tracker tr-5"></div>
+                        <div class="tracker tr-6"></div>
+                        <div class="tracker tr-7"></div>
+                        <div class="tracker tr-8"></div>
+                        <div class="tracker tr-9"></div>
+                        <div class="tracker tr-10"></div>
+                        <div class="tracker tr-11"></div>
+                        <div class="tracker tr-12"></div>
+                        <div class="tracker tr-13"></div>
+                        <div class="tracker tr-14"></div>
+                        <div class="tracker tr-15"></div>
+                        <div class="tracker tr-16"></div>
+                        <div class="tracker tr-17"></div>
+                        <div class="tracker tr-18"></div>
+                        <div class="tracker tr-19"></div>
+                        <div class="tracker tr-20"></div>
+                        <div class="tracker tr-21"></div>
+                        <div class="tracker tr-22"></div>
+                        <div class="tracker tr-23"></div>
+                        <div class="tracker tr-24"></div>
+                        <div class="tracker tr-25"></div>
+                    <div id="card">
+                    <p id="prompt">HOVER OVER :D</p>
+                    <div class="title">look mom,<br/>no JS</div>
+                    <div class="subtitle">
+                        mouse hover tracker
+                    </div>
+                </div>
+             </div>
+            </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Cards
