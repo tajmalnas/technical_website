7ede0a972c480489aeea1424a05faa6fb597e931
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-03-26 16:32:19 +0530

    give name to each card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,7 +3,7 @@ import './Cards.css'
 const Cards = () => {
   return (
     <div className='ancestor'>
-        <div className='rows' style={{paddingLeft:'26px'}}>
+        <div className='rows' style={{paddingLeft:'120px'}}>
             <div className='card'>
                 <div class="container noselect">
                     <div class="canvas">
@@ -34,7 +34,7 @@ const Cards = () => {
                         <div class="tracker tr-25"></div>
                     <div id="card">
                     <p id="prompt">C++</p>
-                    <div class="title"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png" height={'65px'}/></div>
+                    <div class="title"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png" height={'65px'}/><p style={{paddingLeft:'3px'}}> C++</p></div>
                 </div>
              </div>
             </div>
@@ -69,7 +69,7 @@ const Cards = () => {
                         <div class="tracker tr-25"></div>
                     <div id="card">
                     <p id="prompt">Java</p>
-                    <div class="title"><img src="https://www.svgrepo.com/download/184143/java.svg" height={'75px'}/></div>
+                    <div class="title"><img src="https://www.svgrepo.com/download/184143/java.svg" height={'75px'}/> <p style={{paddingLeft:'15px'}}> Java</p></div>
                     
                 </div>
              </div>
@@ -105,8 +105,7 @@ const Cards = () => {
                         <div class="tracker tr-25"></div>
                     <div id="card">
                     <p id="prompt">Python</p>
-                    <div class="title"><img src="https://cdn3d.iconscout.com/3d/premium/thumb/python-6815592-5602757.png?f=webp" height={'75px'} /></div>
-
+                    <div class="title"><img src="https://cdn3d.iconscout.com/3d/premium/thumb/python-6815592-5602757.png?f=webp" height={'75px'} /> <p style={{paddingLeft:'3px'}}> Python</p> </div>
                 </div>
              </div>
             </div>
@@ -140,11 +139,8 @@ const Cards = () => {
                         <div class="tracker tr-24"></div>
                         <div class="tracker tr-25"></div>
                     <div id="card">
-                    <p id="prompt">HOVER OVER :D</p>
-                    <div class="title">look mom,<br/>no JS</div>
-                    <div class="subtitle">
-                        mouse hover tracker
-                    </div>
+                    <p id="prompt">JavaScript</p>
+                    <div class="title"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png" height={'65px'} /> <br/> JavaSript </div>
                 </div>
              </div>
             </div>
@@ -178,48 +174,9 @@ const Cards = () => {
                         <div class="tracker tr-24"></div>
                         <div class="tracker tr-25"></div>
                     <div id="card">
-                    <p id="prompt">HOVER OVER :D</p>
-                    <div class="title">look mom,<br/>no JS</div>
-                    <div class="subtitle">
-                        mouse hover tracker
-                    </div>
-                </div>
-             </div>
-            </div>
-            </div>
-            <div className='card'>
-                <div class="container noselect">
-                    <div class="canvas">
-                        <div class="tracker tr-2"></div>
-                        <div class="tracker tr-1"></div>
-                        <div class="tracker tr-3"></div>
-                        <div class="tracker tr-4"></div>
-                        <div class="tracker tr-5"></div>
-                        <div class="tracker tr-6"></div>
-                        <div class="tracker tr-7"></div>
-                        <div class="tracker tr-8"></div>
-                        <div class="tracker tr-9"></div>
-                        <div class="tracker tr-10"></div>
-                        <div class="tracker tr-11"></div>
-                        <div class="tracker tr-12"></div>
-                        <div class="tracker tr-13"></div>
-                        <div class="tracker tr-14"></div>
-                        <div class="tracker tr-15"></div>
-                        <div class="tracker tr-16"></div>
-                        <div class="tracker tr-17"></div>
-                        <div class="tracker tr-18"></div>
-                        <div class="tracker tr-19"></div>
-                        <div class="tracker tr-20"></div>
-                        <div class="tracker tr-21"></div>
-                        <div class="tracker tr-22"></div>
-                        <div class="tracker tr-23"></div>
-                        <div class="tracker tr-24"></div>
-                        <div class="tracker tr-25"></div>
-                    <div id="card">
-                    <p id="prompt">HOVER OVER :D</p>
-                    <div class="title">look mom,<br/>no JS</div>
-                    <div class="subtitle">
-                        mouse hover tracker
+                    <p id="prompt">Kotlin</p>
+                    <div class="title"><img src="https://download.logo.wine/logo/Kotlin_(programming_language)/Kotlin_(programming_language)-Logo.wine.png" height={'75px'}/>
+                     <p style={{paddingLeft:'20px'}}> Kotlin</p>
                     </div>
                 </div>
              </div>
